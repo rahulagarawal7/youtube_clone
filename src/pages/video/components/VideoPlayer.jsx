b5146308eb6b6ca7567af92bd603cb324cae702a
rahulagarawal7 <156163576+rahulagarawal7@users.noreply.github.com>
2025-09-21 22:38:25 +0530

    added  frame

--- src/pages/video/components/VideoPlayer.jsx
+++ src/pages/video/components/VideoPlayer.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
-const VideoPlayer = ({ thumbnailUrl, title }) => {
+const VideoPlayer = ({ videoId = "j6HGO9OMTvY", title = "YouTube Video" }) => {
   return (
     <div className="relative w-full bg-black rounded-xl overflow-hidden mb-4">
       <div className="aspect-video relative">
-        <img
-          src={thumbnailUrl}
-          alt={title}
-          className="w-full h-full object-cover"
-        />
-        {/* Play Button */}
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="w-20 h-20 bg-red-600 rounded-full flex items-center justify-center cursor-pointer hover:bg-red-700 transition-colors">
-            <div className="w-0 h-0 border-l-[20px] border-l-white border-t-[12px] border-t-transparent border-b-[12px] border-b-transparent ml-1"></div>
-          </div>
-        </div>
+        <iframe
+          width="100%"
+          height="100%"
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=0&rel=0&modestbranding=1`}
+          title={title}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="w-full h-full"
+        ></iframe>
       </div>
     </div>
   );
